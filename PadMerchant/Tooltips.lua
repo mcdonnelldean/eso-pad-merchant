225a9de2eb516d350f2581789c120d926762d395
Dean McDonnell <mcdonnelldean@gmail.com>
2024-01-29 20:55:26 +0000

    Update Tooltips.lua
    Clean up to support stack suggestions

--- PadMerchant/Tooltips.lua
+++ PadMerchant/Tooltips.lua
@@ -6,24 +6,28 @@ local GENERAL_COLOR_GREY = GAMEPAD_TOOLTIP_COLOR_GENERAL_COLOR_2
 local GENERAL_COLOR_OFF_WHITE = GAMEPAD_TOOLTIP_COLOR_GENERAL_COLOR_3
 
 
-local function GenerateTipContent(itemLink)
+local function GenerateTipContent(itemLink, stackCount)
 	local itemInfo = TamrielTradeCentre_ItemInfo:New(itemLink)
 	local pricing = TamrielTradeCentrePrice:GetPriceInfo(itemInfo)
-
 
 	if pricing == nil then
 		pricing = {}
 	end
 
-	local suggested = "No Suggestion Available"
+	local itemSuggestion = ""
+	local stackSuggestion = ""
 	if pricing.SuggestedPrice ~= nil then
 		local suggestedMin = TamrielTradeCentre:FormatNumber(pricing.SuggestedPrice, 0) .. GOLD_ICON
 		local suggestedMax = TamrielTradeCentre:FormatNumber(pricing.SuggestedPrice * 1.25, 0) .. GOLD_ICON
 
-		suggested = suggestedMin .. " to " .. suggestedMax
+		local stackMin = TamrielTradeCentre:FormatNumber(pricing.SuggestedPrice * stackCount, 0) .. GOLD_ICON
+		local stackdMax = TamrielTradeCentre:FormatNumber((pricing.SuggestedPrice * 1.25) * stackCount, 0) .. GOLD_ICON
+
+		itemSuggestion = suggestedMin .. " to " .. suggestedMax
+		stackSuggestion = stackMin .. " to " .. stackdMax
 	end
 
-	local listingsNote = "No historical data available"
+	local listingsNote = "No listing data seen"
 	if pricing.EntryCount ~= nil and pricing.Avg ~= nill then
 		local listings = TamrielTradeCentre:FormatNumber(pricing.EntryCount, 0)
 		local priceAvg = TamrielTradeCentre:FormatNumber(pricing.Avg, 0) .. GOLD_ICON
@@ -31,7 +35,7 @@ local function GenerateTipContent(itemLink)
 		listingsNote = listings .. " listings, averaging " .. priceAvg
 	end
 
-	local salesNote = "No historical data available"
+	local salesNote = "No sales data seen"
 	if pricing.SaleEntryCount ~= nil and pricing.SaleAvg ~= nill then
 		local sales = TamrielTradeCentre:FormatNumber(pricing.SaleEntryCount, 0)
 		local saleAvg = TamrielTradeCentre:FormatNumber(pricing.SaleAvg, 0) .. GOLD_ICON
@@ -39,16 +43,23 @@ local function GenerateTipContent(itemLink)
 		salesNote = sales .. " sales, averaging " .. saleAvg
 	end
 
-	pricing.Suggested = suggested
+	pricing.ItemSuggestion = itemSuggestion
+	pricing.StackSuggestion = stackSuggestion
 	pricing.Sales = salesNote
 	pricing.Listings = listingsNote
+	pricing.StackSize = stackCount
+
+	
 
 	return pricing
 end
 
 -- Adds sales data from MasterMerchant data about to the provided
-local function AddData(tooltip, itemLink)
+local function AddData(tooltip, itemLink, stackCount)
 	if itemLink == nil then return end
+
+	-- Generate our tip content and params
+	local tipContent = GenerateTipContent(itemLink, stackCount)
 
 	-- Our style is common to both header and tip
 	local style = tooltip:GetStyle("bodySection")
@@ -63,34 +74,45 @@ local function AddData(tooltip, itemLink)
         height = 24,
 	}
 
-	-- Generate our tip content and params
-	local tipContent = GenerateTipContent(itemLink)
+
 	local tipParams = {
 		fontColorField = GENERAL_COLOR_WHITE,
         fontFace = "$(GAMEPAD_LIGHT_FONT)",
-        fontSize = "$(GP_34)"
+        fontSize = "$(GP_34)",
+		height = 12
 	}
 
 	local tipParams2 = {
 		fontColorField = GENERAL_COLOR_WHITE,
         fontFace = "$(GAMEPAD_MEDIUM_FONT)",
-        fontSize = "$(GP_42)"
+        fontSize = 38
 	}
 
 
 	local FreqParams = {
-		fontSize = 27,
-		fontColorField = GENERAL_COLOR_WHITE
+		fontSize = 22,
+		uppercase = true,
+		height = 6,
+		fontColorField = GENERAL_COLOR_GREY,
+		fontFace = "$(GAMEPAD_MEDIUM_FONT)",
 	}
 
 	tooltip:AddLine("SUGGESTED PRICING", headerParams, style)
-	tooltip:AddLine(tipContent.Suggested, tipParams2, style)
-	tooltip:AddLine("SALES & LISTINGS", headerParams, style)
+	if tipContent.ItemSuggestion ~= "" then
+		if tipContent.StackSize ~= 1 then
+			tooltip:AddLine("this stack of " .. tipContent.StackSize, FreqParams, style)
+			tooltip:AddLine(tipContent.StackSuggestion, tipParams2, style)	
+		end
+
+		tooltip:AddLine("per unit", FreqParams, style)
+		tooltip:AddLine(tipContent.ItemSuggestion, tipParams2, style)
+	else
+		tooltip:AddLine("No suggestions available", tipParams2, style)
+	end
+
 	tooltip:AddLine(tipContent.Sales, tipParams, style)
 	tooltip:AddLine(tipContent.Listings, tipParams, style)
-
-	tooltip:AddLine("Pricing is per unit.", FreqParams, style)
-
+	tooltip:AddLine("", tipParams, style)
 end
 
 -- Integrates with the given tooltip panel.
@@ -101,9 +123,20 @@ local function IntegrateWith(tooltip)
 	-- The wrapper will call the old method first, we will add our data at the bottom of 
 	-- the list. The original method returns a context that needs to be returned by the 
 	-- wrapper. If this is not returned 'equipped' windows will break see (#3).
-	local function Wrapper(self, itemLink, ...)
-		local context = original(self, itemLink, ...)
-		AddData(self, itemLink)
+	local function Wrapper(self, itemLink, aa,bb,cc,dd,itemName, ...)
+		local context = original(self, itemLink, aa,bb,cc,dd,itemName, ...)
+		local stackCount = 1
+
+		if itemName ~= nil then
+			stackCount = string.match(itemName,"%((%d+)%)")
+
+			if stackCount == nil then
+				stackCount = 1
+			end	
+		end
+
+		AddData(self, itemLink, stackCount)
+
 		return context
 	end
 
